b32bd9b75ead445e357333b313734d9582451556
Zhang Zehua <edward.zehua.zhang@gmail.com>
2025-11-04 23:25:45 +0800

    feat: boss

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 
 function Header() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <header className="header">
       <div className="header-container">
@@ -37,7 +43,7 @@ function Header() {
           </nav>
         </div>
         <div className="header-right">
-          <div className="language-selector">
+          <div className="language-selector desktop-only">
             <span>En</span>
             <svg
               width="16"
@@ -52,11 +58,61 @@ function Header() {
               />
             </svg>
           </div>
-          <a href="#login" className="login-btn">
+          <a href="#login" className="login-btn desktop-only">
             Log in
           </a>
+          
+          {/* Mobile Hamburger Menu */}
+          <button 
+            className="hamburger-menu" 
+            onClick={toggleMobileMenu}
+            aria-label="Toggle menu"
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
         </div>
       </div>
+
+      {/* Mobile Menu Dropdown */}
+      {isMobileMenuOpen && (
+        <div className="mobile-menu-dropdown">
+          <div className="mobile-menu-content">
+            <div className="mobile-nav-item">
+              <span>Pricing</span>
+            </div>
+            <a href="#help" className="mobile-nav-item">
+              Help
+            </a>
+            <a href="#about" className="mobile-nav-item">
+              About
+            </a>
+            <a href="#blog" className="mobile-nav-item">
+              Blog
+            </a>
+            <div className="mobile-menu-divider"></div>
+            <div className="mobile-nav-item">
+              <span>En</span>
+              <svg
+                width="16"
+                height="10"
+                viewBox="0 0 17 10"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M8.60986 9.49994L1.10986 1.99994L2.15986 0.949938L8.60986 7.39994L15.0599 0.949938L16.1099 1.99994L8.60986 9.49994Z"
+                  fill="white"
+                />
+              </svg>
+            </div>
+            <a href="#login" className="mobile-nav-item">
+              Log in
+            </a>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
