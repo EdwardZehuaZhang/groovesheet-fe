9c3b813c0992c82eb55769db42b33e5bcde082c3
Zhang Zehua <edward.zehua.zhang@gmail.com>
2025-11-07 12:04:01 +0800

    fix: nav bar padding

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -47,13 +47,42 @@ function Header() {
   return (
     <header className="header">
       <div className="header-container">
-        <div className="header-left">
-          <div className="logo">
-            <img src="/images/Logo_White.png" alt="DrumScore Logo" className="logo-image" />
+        {/* Inner constrained content to align with main page sections (e.g. Pricing) */}
+        <div className="header-inner">
+          <div className="header-left">
+            <div className="logo">
+              <img src="/images/Logo_White.png" alt="DrumScore Logo" className="logo-image" />
+            </div>
+            <nav className="nav-menu">
+              <div className="nav-item dropdown">
+                <span>Pricing</span>
+                <svg
+                  width="16"
+                  height="10"
+                  viewBox="0 0 17 10"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M8.10986 9.49994L0.609863 1.99994L1.65986 0.949938L8.10986 7.39994L14.5599 0.949938L15.6099 1.99994L8.10986 9.49994Z"
+                    fill="white"
+                  />
+                </svg>
+              </div>
+              <a href="#help" className="nav-item">
+                Help
+              </a>
+              <a href="#about" className="nav-item">
+                About
+              </a>
+              <a href="#blog" className="nav-item">
+                Blog
+              </a>
+            </nav>
           </div>
-          <nav className="nav-menu">
-            <div className="nav-item dropdown">
-              <span>Pricing</span>
+          <div className="header-right">
+            <div className="language-selector desktop-only">
+              <span>En</span>
               <svg
                 width="16"
                 height="10"
@@ -62,62 +91,36 @@ function Header() {
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  d="M8.10986 9.49994L0.609863 1.99994L1.65986 0.949938L8.10986 7.39994L14.5599 0.949938L15.6099 1.99994L8.10986 9.49994Z"
+                  d="M8.60986 9.49994L1.10986 1.99994L2.15986 0.949938L8.60986 7.39994L15.0599 0.949938L16.1099 1.99994L8.60986 9.49994Z"
                   fill="white"
                 />
               </svg>
             </div>
-            <a href="#help" className="nav-item">
-              Help
-            </a>
-            <a href="#about" className="nav-item">
-              About
-            </a>
-            <a href="#blog" className="nav-item">
-              Blog
-            </a>
-          </nav>
-        </div>
-        <div className="header-right">
-          <div className="language-selector desktop-only">
-            <span>En</span>
-            <svg
-              width="16"
-              height="10"
-              viewBox="0 0 17 10"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M8.60986 9.49994L1.10986 1.99994L2.15986 0.949938L8.60986 7.39994L15.0599 0.949938L16.1099 1.99994L8.60986 9.49994Z"
-                fill="white"
-              />
-            </svg>
-          </div>
-          <SignedOut>
-            <button onClick={openLoginModal} className="login-btn desktop-only">
-              Log in
-            </button>
-          </SignedOut>
-          <SignedIn>
-            <div className="desktop-only">
-              <UserButton
-                afterSignOutUrl="/"
-                appearance={{
-                  elements: {
-                    avatarBox: 'w-10 h-10',
-                  },
-                }}
-              />
-            </div>
-          </SignedIn>
+            <SignedOut>
+              <button onClick={openLoginModal} className="login-btn desktop-only">
+                Log in
+              </button>
+            </SignedOut>
+            <SignedIn>
+              <div className="desktop-only">
+                <UserButton
+                  afterSignOutUrl="/"
+                  appearance={{
+                    elements: {
+                      avatarBox: 'w-10 h-10',
+                    },
+                  }}
+                />
+              </div>
+            </SignedIn>
 
-          {/* Mobile Hamburger Menu */}
-          <button className="hamburger-menu" onClick={toggleMobileMenu} aria-label="Toggle menu">
-            <span></span>
-            <span></span>
-            <span></span>
-          </button>
+            {/* Mobile Hamburger Menu */}
+            <button className="hamburger-menu" onClick={toggleMobileMenu} aria-label="Toggle menu">
+              <span></span>
+              <span></span>
+              <span></span>
+            </button>
+          </div>
         </div>
       </div>
 
